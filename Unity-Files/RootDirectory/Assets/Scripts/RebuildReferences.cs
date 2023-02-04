b104cb207ffd185ac74fcd8fa705e73f526cae48
Brendan Kane <54537957+FlerBaDet@users.noreply.github.com>
2023-02-04 17:41:04 -0500

    FileCont

--- Unity-Files/RootDirectory/Assets/Scripts/RebuildReferences.cs
+++ Unity-Files/RootDirectory/Assets/Scripts/RebuildReferences.cs
@@ -1,32 +1,64 @@
 using Mono.Cecil;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class RebuildReferences : MonoBehaviour
 {
-    public string StartPath;
+    public string StartPath = "DirectoriesSAVE";
     public Directory currentDirectory;
     public int lvl = 0;
     public OSController controller;
 
     private void Start()
     {
-        currentDirectory = controller.osNav.currentDirectory;
+        currentDirectory = Resources.Load<Directory>(StartPath + "/root");
+        StartPath = "DirectoriesSAVE";
         traverse(StartPath);
     }
 
     private void traverse(string path)
     {
-        foreach (File file in currentDirectory.files)
+        resetPaths(path, currentDirectory);
+    }
+
+    private void resetFiles(string path, Directory directory)
+    {
+        if (!directory.files.Count.Equals(0))
         {
-            currentDirectory.files.Remove(file);
+            foreach (File file in currentDirectory.files)
+            {
+                currentDirectory.files.Remove(file);
+            }
+            File[] files = Resources.LoadAll<File>(path);
+            foreach (File file in files)
+            {
+                currentDirectory.files.Add(file);
+            }
         }
-        File[] files = Resources.LoadAll<File>(path);
-        foreach (File file in files)
+    }
+
+    private void resetPaths(string path, Directory directory)
+    {
+        //Debug.Log(path);
+        if (!directory.paths.Count.Equals(0)) {
+            foreach (Paths paths in directory.paths)
+            {
+                directory.paths.Remove(paths);
+                directory.paths.Add(new Paths());
+                paths.nextDirectory = Resources.Load<Directory>(path + "/" + paths.keyString + "/" + paths.keyString);
+                paths.keyString = paths.nextDirectory.keyword;
+                paths.nextDirectory.previousDirectory = directory;
+                resetPaths(path + "/" + paths.keyString, paths.nextDirectory);
+                resetFiles(path, paths.nextDirectory);
+            }
+        }
+        else
         {
-            currentDirectory.files.Add(file);
+            return;
         }
+        
     }
 
 }
