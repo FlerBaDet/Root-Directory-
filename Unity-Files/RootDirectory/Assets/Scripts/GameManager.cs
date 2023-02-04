64f86e4112f88f6c900c9cc17c61bc8279f183a0
Ian-Darker <90528854+Ian-Darker@users.noreply.github.com>
2023-02-04 14:08:13 -0500

    Finished Pong Scene
    Game now has working pong game

---
+++ Unity-Files/RootDirectory/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Ball ball;
+    public Text playerScoreText;
+    public Text computerScoreText;
+    public Paddle playerPaddle;
+    public Paddle computerPaddle;
+
+    private int _playerScore;
+    private int _computerScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerScores()
+    {
+        _playerScore++;
+
+        this.playerScoreText.text = _playerScore.ToString();
+        resetRound();
+    }
+
+    public void ComputerScores()
+    {
+        _computerScore++;
+
+        this.computerScoreText.text = _computerScore.ToString();
+        resetRound();
+    }    
+    
+    private void resetRound()
+    {
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+        this.playerPaddle.ResetPosition();
+        this.computerPaddle.ResetPosition();
+    }
+
+}
