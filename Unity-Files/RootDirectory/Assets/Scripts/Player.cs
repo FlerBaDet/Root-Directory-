97d105cb1e1122dd26466c3a2f4e4f61c0d348a8
Ian-Darker <90528854+Ian-Darker@users.noreply.github.com>
2023-02-03 21:56:31 -0500

    Added Basic player movement
    can move left and right, and flip direction facing

---
+++ Unity-Files/RootDirectory/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float moveSpeed = 3f;
+    public bool facingRight = true;
+
+    float inputHorizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (inputHorizontal != 0)
+        {
+            //rb.AddForce(new Vector2(inputHorizontal * moveSpeed, 0f));
+            this.transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (inputHorizontal > 0 && !facingRight)
+        {
+            flip();
+        }
+        else if (inputHorizontal < 0 && facingRight)
+        {
+            flip();
+        }
+
+    }
+    void flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
+    }
+
+}
