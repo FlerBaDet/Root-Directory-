3c285fdf8c2ba66331173fb568c7beef17a2ec8c
Brendan Kane <54537957+FlerBaDet@users.noreply.github.com>
2023-02-05 16:39:19 -0500

    GameFiles -- NEED IMPORT EVENTS

---
+++ Unity-Files/RootDirectory/Assets/Scripts/FileTypes/MP3.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Sod-OS/FILETYPE/MP3")]
+public class MP3 : File
+{
+    public override void Open(OSController controller)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void Open(OSController controller, EventManager eventManager)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void Delete(OSController controller)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+}
